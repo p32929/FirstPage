423712e03e1645b7067897f7d5a1f3f021fb6757
p32929 <p32929@yahoo.com>
2020-09-14 20:50:11 +0600

    Updates

---
+++ precache-manifest.b9d742e576c454a99b3b21dbae5fe960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1603e80b47f68d40e1e9d0c4fe27a9c2",
+    "url": "/FirstPage/index.html"
+  },
+  {
+    "revision": "c0c50467b7b14845be19",
+    "url": "/FirstPage/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "30b2863934b081a9c69d",
+    "url": "/FirstPage/static/js/2.b6158e77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FirstPage/static/js/2.b6158e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c50467b7b14845be19",
+    "url": "/FirstPage/static/js/main.4b1033ba.chunk.js"
+  },
+  {
+    "revision": "e57b22d302a3f8d7d136",
+    "url": "/FirstPage/static/js/runtime-main.74c44492.js"
+  }
+]);
