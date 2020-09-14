05d5ef595388fa0afdcc197d9f6b3817efec321b
p32929 <p32929@yahoo.com>
2020-09-14 21:28:48 +0600

    open links in current tab

--- src/Components/MyLinkItem.js
+++ src/Components/MyLinkItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
-import {useOvermind} from "../Others/OvermindHelper";
+import { useOvermind } from "../Others/OvermindHelper";
 import Button from "@material-ui/core/Button";
 import PropTypes from "prop-types";
-import {SavedDataManager} from "../Others/Globals";
+import { SavedDataManager } from "../Others/Globals";
 
 const MyLinkItem = (props) => {
-    const {item, index} = props;
-    const {state, actions} = useOvermind()
+    const { item, index } = props;
+    const { state, actions } = useOvermind()
 
     return (
         <Grid item item xs={12} sm={12} md={4} lg={4} xl={4}>
@@ -18,9 +18,10 @@ const MyLinkItem = (props) => {
                         actions.deleteUrl(index)
                         SavedDataManager.saveUrls(state)
                     } else {
-                        window.open(item?.link, '_blank');
+                        window.open(item?.link);
+                        // window.open(item?.link, '_blank');
                     }
-                }} style={{width: '100%'}} color='primary'
+                }} style={{ width: '100%' }} color='primary'
                 variant={state.isModeDelete ? 'contained' : 'outlined'}>{item?.title}</Button>
         </Grid>
     );
